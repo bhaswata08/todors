1d69714a4642a6e7362b8881b8b630911bd41ed8
Bhaswata Choudhury <eeb20007@tezu.ac.in>
2025-07-17 18:42:25 +0530

    Added listing of completed todos, listing of pending todos, editor support, listing of workspaces

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+// WIP
 use std::env;
 use std::fs;
 use std::io::{prelude::*};
+use std::process::Command;
 use std::path::Path;
 use serde_derive::{Deserialize, Serialize};
-use clap::{command, Parser};
+use clap::{command, Parser, ValueEnum};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, ValueEnum)]
 enum Priority {
     Low,
     Medium,
@@ -35,11 +37,11 @@ impl Priority {
     fn from_markdown(line:&str) -> Priority {
         if line.contains("{URGENT}") {
             Priority::Urgent
-        } else if line.contains("{High}") {
+        } else if line.contains("{HIGH}") {
             Priority::High
-        } else if line.contains("{Medium}") {
+        } else if line.contains("{MEDIUM}") {
             Priority::Medium
-        } else if line.contains("{Low}") {
+        } else if line.contains("{LOW}") {
             Priority::Low
         } else {
             Priority::Medium
@@ -55,11 +57,19 @@ impl Priority {
     }
 }
 
+
+#[derive(Debug,PartialEq, PartialOrd, Clone, Serialize, Deserialize)]
+enum StatusFilter {
+    All,
+    Completed,
+    Pending,
+}
+
 impl TodoManager {
 
     fn new(file_path: String) -> Self {
         if let Some(parent) = Path::new(&file_path).parent() {
-            fs::create_dir_all(&parent);
+            fs::create_dir_all(&parent).expect("Could not create config directory");
         };
         let mut manager = TodoManager {
             file_path,
@@ -67,7 +77,8 @@ impl TodoManager {
             current_space: None
         };
         if manager.load_todos().is_err() {
-            manager.current_todo_spaces.push(TodoSpace { name: "Default".to_string(), todos: Vec::new() })
+            manager.current_todo_spaces.push(TodoSpace { name: "Default".to_string(), todos: Vec::new() });
+            manager.save_todos().expect("Could not create initial todo file");
         }
         manager
     }
@@ -97,15 +108,28 @@ impl TodoManager {
         
     }
 
-    fn list_todos(&mut self) {
+    fn list_todos(&mut self, todo_state: StatusFilter) {
         for space in &self.current_todo_spaces {
-            println!{"=== {} ===", space.name};
-            for (i, todo) in space.todos.iter().enumerate() {
+            println!("=== {} ===", space.name);
+            let todos_to_display: Vec<_> = match todo_state {
+                StatusFilter::All => {
+                    space.todos.iter().enumerate().collect()
+                },
+                StatusFilter::Completed => {
+                    space.todos.iter().enumerate().filter(|&(_, todo)| todo.status).collect()
+                },
+                StatusFilter::Pending => {
+                    space.todos.iter().enumerate().filter(|&(_, todo)| !todo.status).collect()
+                },
+            };
+
+            for (i, todo) in todos_to_display {
                 let checkbox = if todo.status {"[x]"} else {"[ ]"};
-                println!("{}: {} {} ({:?})", i, checkbox, todo.item, todo.priority);
+                println!("- {}: {} {} {}", i, checkbox, todo.item, todo.priority.to_markdown());
             }
             println!();
         }
+
     }
 
     fn toggle_todo(&mut self, index: usize, space_name: Option<String>) -> Result<(), String> {
@@ -137,19 +161,33 @@ impl TodoManager {
     }
 
     fn save_todos(&self) -> Result<(), String> {
-        let json_string = serde_json::to_string(&self.current_todo_spaces)
-                .map_err(|e| e.to_string())?;
-
-        fs::write(&self.file_path, json_string).map_err(|e| e.to_string())?;
+        let md_string = format_todos_as_markdown(&self.current_todo_spaces);
+        fs::write(&self.file_path, md_string).map_err(|e| e.to_string())?;
         Ok(())
     }
 
     fn load_todos(&mut self) -> Result<(), String> {
-        let json_string = fs::read_to_string(&self.file_path)
+        let md_string = fs::read_to_string(&self.file_path)
             .map_err(|e| e.to_string())?;
-        let loaded_spaces: Vec<TodoSpace> = serde_json::from_str(&json_string)
-            .map_err(|e| e.to_string())?;
+        let loaded_spaces: Vec<TodoSpace> = parse_markdown_todos(md_string);
         self.current_todo_spaces = loaded_spaces;
+        Ok(())
+    }
+    fn edit(&self) -> Result<(), String> {
+        let editor = env::var("EDITOR").unwrap_or("vim".to_string());
+        Command::new(editor).args([&self.file_path]).spawn()
+            .map_err(|e| e.to_string())?
+            .wait()
+            .map_err(|e| e.to_string())?;
+        Ok(())
+    }
+
+    fn list_workspaces(&self) -> Result<(), String> {
+        for spaces in self.current_todo_spaces.iter() {
+            let completed: usize = spaces.todos.iter().filter(|todo| todo.status).count();
+            let total = spaces.todos.len();
+            println!("{} ({completed}/{total} completed)", spaces.name);
+        }
         Ok(())
     }
 }
@@ -204,17 +242,17 @@ fn parse_markdown_todos(content: String) -> Vec<TodoSpace> {
         }
 
     }
-    if current_space.todos.is_empty() || current_space.name != "Default" {
+    if !current_space.todos.is_empty() || current_space.name != "Default" {
         spaces.push(current_space)
     }
     spaces
 }
 
-fn format_todos_as_markdown(spaces: Vec<TodoSpace>) -> String {
+fn format_todos_as_markdown(spaces: &[TodoSpace]) -> String {
     let mut markdown_string = String::new();
     for space in spaces.iter() {
         if space.name != "Default" {
-            markdown_string.push_str(&format!("[[{}]]", space.name));
+            markdown_string.push_str(&format!("[[{}]]\n", space.name));
         }
         for todo in space.todos.iter() {
             let checkbox = if todo.status { "[x]" } else { "[ ]" };
@@ -229,44 +267,83 @@ fn format_todos_as_markdown(spaces: Vec<TodoSpace>) -> String {
     markdown_string
 }
 
+#[derive(Debug, Clone, ValueEnum)]
+enum ActionArgs {
+    New, //unsure
+    Toggle, //done
+    Edit,
+    Delete, //done
+    Add, //done
+    List, //done
+    ListCompleted, //done
+    ListPending, //done
+    ListSpaces, // done
+}
+
 
 #[derive(Parser)]
 #[command(name = "todo")]
 #[command(about = "A simple todo manager")]
 struct Cli {
-    #[arg(short, long)]
-    add: Option<String>,
+    #[arg(value_enum)]
+    action: ActionArgs,
 
     #[arg(short, long)]
-    toggle: Option<usize>,
+    todo: Option<String>,
+    
+    #[arg(short, long, default_value_t=0)]
+    index: usize,
+
+    #[arg(short, long, value_enum)]
+    priority: Option<Priority>,
 
     #[arg(short, long)]
-    delete: Option<usize>,
+    space_name: Option<String>,
 
     #[arg(short, long)]
-    list: bool,
+    filename:Option<String>
 }
 
 fn main() -> Result<(), String> {
     let args = Cli::parse();
     let config_path = env::var("XDG_CONFIG_HOME").expect("$XDH_CONFIG_PATH not set");
-    let path = config_path + "/todo" + "/todos.json";
+    let path = config_path + "/todo" + "/todos.md";
 
     let mut manager = TodoManager::new(path.to_string());
-
-    if let Some(todo_text) = args.add {
-        manager.add_todo(todo_text, None, None)?;
-        println!("Todo added!");
-    }
-    if let Some(idx) = args.toggle {
-        manager.toggle_todo(idx, None)?;
-        println!("Todo toggled!");
-    }
-    if let Some(idx) = args.delete {
-        manager.delete_todo(idx, None)?;
-    }
-    if args.list {
-        manager.list_todos();
+    match args.action {
+        ActionArgs::Add => {
+            if let Some(todo_text) = args.todo {
+                manager.add_todo(todo_text, args.space_name, args.priority)?;
+            } else {
+                println!("Please provide todo with --todo");
+            }
+        },
+        ActionArgs::Toggle => {
+            manager.toggle_todo(args.index, args.space_name)?;
+        },
+        ActionArgs::Delete => {
+            manager.delete_todo(args.index, args.space_name)?;
+        },
+        ActionArgs::List => {
+            manager.list_todos(StatusFilter::All);
+        },
+        ActionArgs::ListCompleted => {
+            manager.list_todos(StatusFilter::Completed);
+        },
+        ActionArgs::ListPending => {
+            manager.list_todos(StatusFilter::Pending);
+        },
+        ActionArgs::New => {
+            let _manager = TodoManager::new(path.to_string());
+            println!("Todo manager initialized at: {}", path);
+        },
+        ActionArgs::Edit => {
+            manager.edit();
+        },
+        ActionArgs::ListSpaces => {
+            manager.list_workspaces();
+            
+        }
     }
     Ok(())
 
